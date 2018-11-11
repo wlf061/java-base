40a7ff1071008dfe7919bd7ca4ce79a5e81be3ac
nancy.wang <wlf061@sina.com>
2018-11-11 21:05:14 +0800

    1. study of chapter2

---
+++ HighConcurrency/src/com.wlf.study.concurrency/chapter2/CreateThread.java
@@ -0,0 +1,16 @@
+package com.wlf.study.concurrency.chapter2;
+
+
+class ThreadRunnale implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("I'm test1 thread");
+    }
+}
+public class CreateThread {
+    public static void main(String[] str) {
+        Thread t1 = new Thread(new ThreadRunnale());
+        t1.start();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-base
+### java 并发基础
+![avatar](http://mm.edrawsoft.cn/map.html?obj=qqFCC81004664773D497B27376325CCB67/Personal/java%E9%AB%98%E5%B9%B6%E5%8F%91/java%E5%A4%9A%E7%BA%BF%E7%A8%8B%E5%9F%BA%E7%A1%80.emmx)
